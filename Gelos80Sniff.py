3f6adc2e0056f31aacab30d61b7320ad3677b8c8
Rayyan (Ray) Hodges <rayyan.hodges@studytafensw.edu.au>
2025-03-17 15:43:19 +1100

    Various edits
    Signed-off-by: Rayyan (Ray) Hodges <rayyan.hodges@studytafensw.edu.au>

--- Gelos80Sniff.py
+++ Gelos80Sniff.py
@@ -2,4 +2,7 @@
 # rayyan.hodges@studytafensw.edu.au
 # This program is coded in Python and designed to scan the local network for any clients connected that have port 80 open.
 # If a machine is found to have port 80 open, proceed to use programs like GoBuster and dirb to check for hidden directories without proper security in place and exploit.
+# Results are exported to a CSV file for convenience.
 
+# Required modules to import
+import csv
